24fe307d29ea17514a87e7fbb8a86abf2e815f83
your_name <orkunkorcak@gmail.com>
2025-07-22 23:35:43 +0300

    start vite organizasion

---
+++ src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import css from './App.module.css';
+
+function App() {
+  return (
+    <div className={css.appContainer}>
+      <h1>Welcome to Slim Moms App</h1>
+      <p>Comming Soon...</p>
+    </div>
+  );
+}
+export default App;
