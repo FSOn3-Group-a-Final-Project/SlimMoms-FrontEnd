c30649b788771347dfbf861deae4c83acbaa0e85
Nilay Türker <nilaytr89qgmail.com>
2025-07-26 18:57:41 +0300

    register form update

---
+++ src/components/registerForm/RegisterForm.jsx
@@ -0,0 +1,104 @@
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+//import css from './RegisterForm.module.css';
+
+const RegisterForm = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const initialValues = {
+        name: '',
+        email: '',
+        password: '',
+    };
+
+    const handleSubmit = (values, { resetForm, setSubmitting }) => {
+        try {
+            const { name, email, password } = values;
+            toast.success('Registration is successful!');
+
+            resetForm();
+            navigate('/home');
+        } catch (error) {
+            if (error.code === 11000) {
+                toast.error('User with this email already exists.');
+            } else {
+                toast.error('Registration failed, please try again.');
+            }
+            resetForm();
+        } finally {
+            setSubmitting(false);
+        }
+    };
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required('Name is required').min(3, 'Name must be at least 3 characters'),
+        email: Yup.string().required('Email is required').email('Invalid email address'),
+        password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters'),
+    });
+
+    return (
+        <div>
+            <Toaster position="top-center" reverseOrder={false} />
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <h1>REGISTER</h1>
+                        <div>
+                            <Field
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Name *"
+                            />
+                            <ErrorMessage
+                                name="name"
+                            />
+                        </div>
+
+                        <div>
+                            <Field
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="Email *"
+                            />
+                            <ErrorMessage
+                                name="email"
+                            />
+                        </div>
+
+                        <div>
+                            <Field
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Password *"
+                            />
+                            <ErrorMessage
+                                name="password"
+                            />
+                        </div>
+
+                        <div>
+                            <button type="submit" disabled={isSubmitting}>Register</button>
+                        </div>
+
+                        <div>
+                            <button type="button" onClick={() => navigate('/login')}>Log in</button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+export default RegisterForm;
