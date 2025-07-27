a34e425518811ee5725b41ba0207c5d7279b3a17
your_name <orkunkorcak@gmail.com>
2025-07-27 16:56:31 +0300

    loginform content added

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,14 +1,77 @@
-import css from './LoginForm.module.css';
-import { Field, Form, Formik } from "formik";
-import { useDispatch } from "react-redux";
+import { logIn } from "../../redux/auth/operations";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import css from "./LoginForm.module.css";
 
+const LoginForm = () => {
+  const dispatch = useDispatch();
 
+  const initialValues = {
+    email: "",
+    password: "",
+  };
 
-const LoginForm = ({ onSubmit }) => {
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string()
+      .min(6, "Must be at least 6 characters")
+      .required("Required"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      await dispatch(logIn(values));
+    } catch (error) {
+      console.error("Login error:", error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <Formik></Formik>
-
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className={css.form}>
+            <div className={css.field}>
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" className={css.input} />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.error}
+              />
+            </div>
+            <div className={css.field}>
+              <label htmlFor="password">Password</label>
+              <Field type="password" name="password" className={css.input} />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={css.error}
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={css.button}
+            >
+              {isSubmitting ? "Loading..." : "Log In"}
+            </button>
+            <button>
+              type="submit"
+              disabled={isSubmitting}
+              className={css.buttonTwo}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
-}
+};
 export default LoginForm;
