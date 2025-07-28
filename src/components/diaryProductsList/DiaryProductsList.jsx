2f51a66f24d610d65b33588ef8a2f25bc54beef9
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-07-29 01:29:16 +0300

    diaryproductslist fixed

--- src/components/diaryProductsList/DiaryProductsList.jsx
+++ src/components/diaryProductsList/DiaryProductsList.jsx
@@ -4,9 +4,9 @@ import DiaryProductsListItem from "../DiaryProductsListItem/DiaryProductsListIte
 import styles from "./DiaryProductsList.module.css";
 
 const DiaryProductsList = () => {
-  const products = useSelector(selectDiaryProductsByDate);
-console.log("DiaryProductsList products:", products);
-  if (!products || products.length === 0) {
+  const products = useSelector(selectDiaryProductsByDate) || [];
+  console.log("DiaryProductsList products:", products);
+  if (!products.length) {
     return <p className={styles.empty}>Product not added yet</p>;
   }
 
