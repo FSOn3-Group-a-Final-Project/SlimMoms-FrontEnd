46b035aa62404b114583036400df305e4d0368e1
dreamer981 <sdreamer590@gmail.com>
2025-07-28 19:47:37 +0300

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/app/App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import {store }from "./redux/store";
 // import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
