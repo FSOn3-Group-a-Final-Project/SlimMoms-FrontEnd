c2cb86ec185887322f3e606480668845ac5d3660
dreamer981 <sdreamer590@gmail.com>
2025-07-29 14:53:15 +0300

    diarydatecalendar value olarak selecteddate eklendi

--- src/components/diaryDateCalendar/DiaryDateСalendar.jsx
+++ src/components/diaryDateCalendar/DiaryDateСalendar.jsx
@@ -1,16 +1,17 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedDate } from '../../redux/diary/slice.js';
+import { selectSelectedDate } from '../../redux/diary/selectors.js';
 
 const DiaryDateCalendar = () => {
   const dispatch = useDispatch();
-
+ const selectedDate = useSelector(selectSelectedDate);
   const handleDateChange = (event) => {
     const chosenDate = event.target.value; // yyyy-mm-dd formatında 
     dispatch(setSelectedDate(chosenDate));
   };
 
   return (
-    <input type="date" onChange={handleDateChange} />
+    <input type="date" value={selectedDate} onChange={handleDateChange} />
   );
 };
 
