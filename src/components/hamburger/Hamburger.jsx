4b2af3744f256d1da0741a783a4ece571acd0682
your_name <orkunkorcak@gmail.com>
2025-08-04 22:20:48 +0300

    hamburger  ayarlandı

--- src/components/hamburger/Hamburger.jsx
+++ src/components/hamburger/Hamburger.jsx
@@ -1,26 +1,38 @@
-import css from './Hamburger.module.css';
-import { useState } from 'react';
-import hamburger from '/images/hamburger.png?url';
-import hamburgerClose from '/images/hamburgerClose.png?url';
-import HamburgerModal from '../hamburgerModal/HamburgerModal';
+import css from "./Hamburger.module.css";
+import { useState } from "react";
+import hamburger from "/images/hamburger.png?url";
+import hamburgerClose from "/images/hamburgerClose.png?url";
+import HamburgerModal from "../hamburgerModal/HamburgerModal";
 import useMedia from "../../hooks/useMedia";
 
 const Hamburger = () => {
-    const { isTablet, isDesktop } = useMedia();
-    const [isHamburgerModalOpen, setIsHamburgerModalOpen] = useState(false);
+  const { isTablet, isDesktop } = useMedia();
+  const [isHamburgerModalOpen, setIsHamburgerModalOpen] = useState(false);
 
-    const toggleHamburgerModal = () => {
-        setIsHamburgerModalOpen(prev => !prev);
-    }
+  const toggleHamburgerModal = () => {
+    setIsHamburgerModalOpen((prev) => !prev);
+  };
 
-    return(
-        <div className={css.hamburgerMainContainer}>
-            <div onClick={toggleHamburgerModal} className={`${css.HamburgerContainer} ${isDesktop ? css.HamburgerContainerDesktop : ''} ${isTablet ? css.HamburgerContainerTablet : ''}`}>
-                {isHamburgerModalOpen ? <img src={hamburgerClose}></img>  : <img src={hamburger}></img>}
-            </div>
-            {isHamburgerModalOpen ? <HamburgerModal/> : ''}
-        </div>
-    )
+  return (
+    <div className={css.hamburgerMainContainer}>
+      <div
+        onClick={toggleHamburgerModal}
+        className={`${css.HamburgerContainer} ${
+          isDesktop ? css.HamburgerContainerDesktop : ""
+        } ${isTablet ? css.HamburgerContainerTablet : ""}`}
+      >
+        {isHamburgerModalOpen ? (
+          <img src={hamburgerClose} />
+        ) : (
+          <img src={hamburger} />
+        )}
+      </div>
+
+      {isHamburgerModalOpen && (
+        <HamburgerModal onClose={() => setIsHamburgerModalOpen(false)} />
+      )}
+    </div>
+  );
 };
 
 export default Hamburger;
