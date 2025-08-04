2d04ebedc78d320ff93f35daf027a61ee7f9900f
your_name <orkunkorcak@gmail.com>
2025-08-05 00:22:12 +0300

    css düzenlemesi yapıldı

--- src/components/rightSidebar/RightSidebar.jsx
+++ src/components/rightSidebar/RightSidebar.jsx
@@ -1,32 +1,30 @@
-import { useSelector } from "react-redux"; // Redux verisi çekmek için
+import { useSelector } from "react-redux";
 import styles from "./RightSidebar.module.css";
 import {
   selectSelectedDate,
   selectDiarySummary,
 } from "../../redux/diary/selectors";
-import { selectDailyCalories, selectForbiddenProducts } from "../../redux/calculator/selectors";
-// import { selectBloodType } from "../../redux/auth/selectors"; // kan grubu selectorü
+import {
+  selectDailyCalories,
+  selectForbiddenProducts,
+} from "../../redux/calculator/selectors";
+// import { selectBloodType } from "../../redux/auth/selectors";
 
 function RightSidebar() {
-  const selectedDate = useSelector(selectSelectedDate); // tarih
-  // const products = useSelector(selectDiaryProductsByDate); // o tarihteki ürünler
-  const dailyCalories = useSelector(selectDailyCalories); // günlük kalori
-  const forbiddenProducts = useSelector(selectForbiddenProducts); // önerilmeyen ürünler
+  const selectedDate = useSelector(selectSelectedDate);
+  const dailyCalories = useSelector(selectDailyCalories);
+  const forbiddenProducts = useSelector(selectForbiddenProducts);
   const { consumed } = useSelector(selectDiarySummary);
 
-  // const bloodType = useSelector(selectBloodType); // kan grubu
-
-
-  const dailyRate = dailyCalories || 0; // Eğer Redux'tan kalori gelmezse varsayılan değer
+  const dailyRate = dailyCalories || 0;
   const left = dailyRate - consumed;
   const percentOfNormal = Math.round((consumed / dailyRate) * 100);
 
   const notRecommended = Array.isArray(forbiddenProducts)
     ? forbiddenProducts.slice(0, 4)
     : [];
-  
-    return (
 
+  return (
     <aside className={styles.Sidebar}>
       <div className={styles.SidebarSummary}>
         <p className={styles.Date}>Summary for {selectedDate}</p>
@@ -57,32 +55,15 @@ function RightSidebar() {
             notRecommended.map((item, index) => (
               <li key={index} className={styles.ProductItem}>
                 {item}
-
               </li>
-              <li>
-                <span className={styles.Label}>% of normal</span>
-                <span className={styles.Value}>{percentOfNormal} %</span>
-              </li>
-            </ul>
-          </div>
-
-          <div className={styles.NotRecommended}>
-            <p className={styles.NotRecommendedLabel}>Food not recommended</p>
-            <ul className={styles.ProductList}>
-              {notRecommended.length > 0 ? (
-                notRecommended.map((item, index) => (
-                  <li key={index} className={styles.ProductItem}>
-                    {item}
-                  </li>
-                ))
-              ) : (
-                <li className={styles.ProductItem}>No data</li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </aside>
-    );
+            ))
+          ) : (
+            <li>No forbidden products</li>
+          )}
+        </ul>
+      </div>
+    </aside>
+  );
 }
 
 export default RightSidebar;
